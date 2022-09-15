db2e82df7d6035040731e6d396c10ead6e908eda
joba <jobtennis21@gmail.com>
2022-09-15 06:40:28 -1200

    aee

--- pages/[...all].vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1 class="text-4xl text-center mb-96">status 404, page not found <br>Redirecting...</h1>
-</template>
-<script setup>
-    import { useRouter } from 'vue-router';
-
-    const router = useRouter()
-    //router.push('/')
-</script>
