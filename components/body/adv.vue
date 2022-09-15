6f2efa32e0175a252c90257b5accd45322977dfe
joba <jobtennis21@gmail.com>
2022-09-15 05:49:34 -1200

    testt

--- components/body/adv.vue
+++ components/body/adv.vue
@@ -60,6 +60,6 @@
 </template>
 <script setup>
     import { HomeIcon, PhoneIcon, MenuIcon, XIcon } from "@heroicons/vue/solid";
-    
+    import { AdjustmentsIcon } from '@heroicons/vue/solid';
     const showNav = ref(false)
     </script>
--- components/body/services.vue
+++ components/body/services.vue
@@ -80,6 +80,7 @@
 </template>
 <script setup>
     import { HomeIcon, PhoneIcon, MenuIcon, XIcon } from "@heroicons/vue/solid";
+    import { AdjustmentsIcon } from '@heroicons/vue/solid';
     
     const showNav = ref(false)
     </script>
